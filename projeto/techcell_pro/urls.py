c2332464aa74eaa19a39b71b76c3d56c8bfaba9a
flaviogui <flavioglaydson-pc@hotmail.com>
2024-07-17 19:11:29 -0300

    Testando tela no servidor

--- projeto/techcell_pro/urls.py
+++ projeto/techcell_pro/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin  # type: ignore
 from django.urls import path, include   # type: ignore
+from django.views.generic import TemplateView # type: ignore
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name="index.html")),
     path('cliente/', include('clienteApp.urls'))
 ]
