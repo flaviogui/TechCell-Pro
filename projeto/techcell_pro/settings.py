939bd43eeba142f2ce35d984bbb180678c44aa3d
flaviogui <flavioglaydson-pc@hotmail.com>
2024-07-25 12:15:15 -0300

    Iniciando slide #22

--- projeto/techcell_pro/settings.py
+++ projeto/techcell_pro/settings.py
@@ -129,7 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,"static")
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
