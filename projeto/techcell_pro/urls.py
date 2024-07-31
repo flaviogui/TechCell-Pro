955cbe7b89799e1ba7ac0755615d95dbedc3ebda
flaviogui <flavioglaydson-pc@hotmail.com>
2024-07-30 21:35:13 -0300

    corrigindo urls repetidas

--- projeto/techcell_pro/urls.py
+++ projeto/techcell_pro/urls.py
@@ -23,6 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cliente/', include('clienteApp.urls')),
     path('aparelho/', include('aparelhoApp.urls')),
-    path('', TemplateView.as_view(template_name="index.html")),
-    path('cliente/', include('clienteApp.urls'))
+    path('', TemplateView.as_view(template_name="index.html"))
 ]
