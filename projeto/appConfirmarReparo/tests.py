1df79eea8b2e321bba33ca1d2c90615a36ce6021
Gutemsam <thesam0606@gmail.com>
2024-07-31 20:18:34 -0300

    atualizando forms, apps views e tests

--- projeto/appConfirmarReparo/tests.py
+++ projeto/appConfirmarReparo/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from audioop import reverse
+from django.test import TestCase, Client # type: ignore
+from django.test import TestCase # type: ignore
+from appConfirmarReparo.models import Reparo
+from appAparelho.models import Aparelho # type: ignore
 
-# Create your tests here.
+
+class ReparoIntegrationTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.aparelho = Aparelho.objects.create(
+            nome='Notebook',
+            descricao='Notebook de alta performance',
+            marca='MarcaZ',
+            modelo='ModeloX',
+            imei='111111111111111',
+            numero_serie='SN1111111111',
+            descricao_problema='Problema com o teclado'
+        )
+        self.reparo = Reparo.objects.create(
+            aparelho=self.aparelho,
+            descricao_problema='Problema com o teclado',
+            custo_estimado=200.00,
+        )
+
+    def test_confirmar_reparo_view(self):
+        # Confirmação do reparo
+        response = self.client.post(reverse('confirmar_reparo', args=[self.reparo.pk]), {
+            'custo_estimado': 250.00,
+            'descricao_problema': 'Teclado trocado e problema resolvido'
+        })
+        self.reparo.refresh_from_db()
+        self.assertEqual(self.reparo.status, 'concluido')
+        self.assertEqual(response.status_code, 302)  # Redirecionamento após a confirmação
+
+    def test_view_reparo_status(self):
+        response = self.client.get(reverse('reparo_detalhes', args=[self.reparo.pk]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Notebook de alta performance')
+        self.assertContains(response, 'Problema com o teclado')
--- projeto/appConfirmarReparo/views.py
+++ projeto/appConfirmarReparo/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect # type: ignore
+from django.utils import timezone # type: ignore
+from .models import Reparo
+from .forms import ConfirmarReparoForm
 
-# Create your views here.
+def confirmar_reparo(request, pk):
+    reparo = get_object_or_404(Reparo, pk=pk)
+    if request.method == 'POST':
+        form = ConfirmarReparoForm(request.POST, instance=reparo)
+        if form.is_valid():
+            reparo = form.save(commit=False)
+            reparo.status = 'concluido'
+            reparo.data_conclusao = timezone.now()
+            reparo.save()
+            # Notificar o cliente
+            notificar_cliente(reparo) # type: ignore
+            return redirect('reparo_detalhes', pk=reparo.pk)
+    else:
+        form = ConfirmarReparoForm(instance=reparo)
+    return render(request, 'confirmar_reparo.html', {'form': form, 'reparo': reparo})
