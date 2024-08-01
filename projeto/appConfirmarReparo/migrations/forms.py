55d45147fd1ef7479b60cf2047913ba75e1cfce3
Gabriel Aquino <92870223+gbrl08snts@users.noreply.github.com>
2024-08-01 14:47:03 -0300

    Delete projeto/appConfirmarReparo directory

--- projeto/appConfirmarReparo/migrations/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms # type: ignore
-from .models import Reparo # type: ignore
-
-class ConfirmarReparoForm(forms.ModelForm):
-    class Meta:
-        model = Reparo
-        fields = ['custo_estimado', 'descricao_problema']
