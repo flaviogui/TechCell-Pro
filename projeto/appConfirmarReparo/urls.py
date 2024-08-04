5310132c20302337f978b49f49b7372ad6d7a120
Gutemsam <thesam0606@gmail.com>
2024-08-04 15:50:08 -0300

    adicionando lista no reparo

--- projeto/appConfirmarReparo/urls.py
+++ projeto/appConfirmarReparo/urls.py
@@ -5,6 +5,7 @@ app_name = 'appConfirmarReparo'
 
 
 urlpatterns = [
+    path('lista_reparos', views.lista_reparos, name='lista_reparos'),
     path('reparo/<int:pk>/', views.reparo_detalhes, name='reparo_detalhes'),
     path('confirmar_reparo/<int:pk>/', views.confirmar_reparo, name='confirmar_reparo'),
     
