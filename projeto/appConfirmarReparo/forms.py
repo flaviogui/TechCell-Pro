a81136a8fc260888ce48a059629f636f5ae6a34b
Gutemsam <thesam0606@gmail.com>
2024-08-06 13:44:47 -0300

    corrigindo bugs

--- projeto/appConfirmarReparo/forms.py
+++ projeto/appConfirmarReparo/forms.py
@@ -1,7 +1,11 @@
 from django import forms # type: ignore
+
+from aparelhoApp.models import Aparelho # type: ignore
 from .models import Reparo # type: ignore
 
 class ConfirmarReparoForm(forms.ModelForm):
+    aparelho = forms.ModelChoiceField(queryset=Aparelho.objects.all(), label="Aparelho")
+
     class Meta:
         model = Reparo
         fields = ['status', 'data_inicio', 'data_conclusao', 'custo_estimado', 'cliente_notificado']
