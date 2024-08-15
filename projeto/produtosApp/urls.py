9ef41600d409d7570731abbb286c38b490c2204b
Gutemsam <thesam0606@gmail.com>
2024-08-15 15:09:40 -0300

    atualizando

--- projeto/produtosApp/urls.py
+++ projeto/produtosApp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path # type: ignore
 from . import views
 
 urlpatterns = [
-    path('produtos/', views.listar_produtos, name='listar_produtos'),
-    path('produto/<int:pk>/', views.visualizar_produto, name='visualizar_produto'),
-    path('produto/novo/', views.criar_produto, name='criar_produto'),
-    path('produto/<int:pk>/editar/', views.editar_produto, name='editar_produto'),
-    path('produto/<int:pk>/excluir/', views.excluir_produto, name='excluir_produto'),
+    path('', views.listar_produtos, name='listar_produtos'),
+    path('<int:pk>/', views.visualizar_produto, name='visualizar_produto'),
+    path('novo/', views.criar_produto, name='criar_produto'),
+    path('<int:pk>/editar/', views.editar_produto, name='editar_produto'),
+    path('<int:pk>/excluir/', views.excluir_produto, name='excluir_produto'),
 ]
