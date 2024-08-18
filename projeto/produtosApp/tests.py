088e2a64f66cb6b7ef6b8e109009f725d2739649
Gutemsam <thesam0606@gmail.com>
2024-08-18 17:49:24 -0300

    atualizando

--- projeto/produtosApp/tests.py
+++ projeto/produtosApp/tests.py
@@ -1,9 +1,10 @@
+from decimal import Decimal
 from django.test import TestCase, Client # type: ignore
 from uuid import uuid4
 from .models import Produto
-<<<<<<< HEAD
 from .forms import ProdutoForm
 from . import views
+from django.urls import reverse, resolve # type: ignore
 
 
 class ProdutoFormTest(TestCase):
@@ -64,16 +65,6 @@ class ProdutoFormTest(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn('codigo_barras', form.errors)
 
-=======
-from produtosApp.forms import ProdutoForm
-from django.urls import reverse, resolve  # type: ignore
-from .views import (
-    produto_create_view,
-    produto_list_view,
-    produto_update_view,
-    produto_delete_view,
-)
->>>>>>> ade1cd5856bba61e60f916571a9c5ed28f9ea520
 
 class ProdutoModelTest(TestCase):
     
@@ -109,7 +100,6 @@ class ProdutoModelTest(TestCase):
         """Testa a representação em string do modelo Produto."""
         self.assertEqual(str(self.produto), "Capinha de celular - Capinha de celular muito boa")
 
-<<<<<<< HEAD
 class URLTests(TestCase):
 
     def test_listar_produtos_url_resolves(self):
@@ -119,61 +109,14 @@ class URLTests(TestCase):
     def test_visualizar_produto_url_resolves(self):
         url = reverse('produto:visualizar_produto', args=[1])
         self.assertEqual(resolve(url).func, views.visualizar_produto)
-=======
-    def test_meta_options(self):
-        """Testa se as opções de Meta estão funcionando corretamente."""
-        meta = Produto._meta
-        self.assertEqual(meta.verbose_name, 'Produto')
-        self.assertEqual(meta.verbose_name_plural, 'Produtos')
-        self.assertEqual(meta.ordering, ['nome', 'preco'])
->>>>>>> ade1cd5856bba61e60f916571a9c5ed28f9ea520
 
     def test_criar_produto_url_resolves(self):
         url = reverse('produto:criar_produto')
         self.assertEqual(resolve(url).func, views.criar_produto)
 
-<<<<<<< HEAD
     def test_editar_produto_url_resolves(self):
         url = reverse('produto:editar_produto', args=[1])
         self.assertEqual(resolve(url).func, views.editar_produto)
-=======
-class ProdutoURLTest(TestCase):
-    
-    def test_create_url(self):
-        path = reverse('produto:create_produto')
-        self.assertEqual(resolve(path).view_name, 'produto:create_produto')
-        self.assertEqual(resolve(path).func, produto_create_view)
-    
-    def test_list_url(self):
-        path = reverse('produto:list_produto')
-        self.assertEqual(resolve(path).view_name, 'produto:list_produto')
-        self.assertEqual(resolve(path).func, produto_list_view)
-
-    def test_update_url(self):
-        produto = Produto.objects.create(
-            nome='Capinha de smartphone',
-            descricao='Capinha de smartphone muito boa',
-            cod_barras='123456789088',
-            preco=11.99,
-            categoria='Produtos diversos'
-        )
-        path = reverse('produto:update_produto', args=[produto.pk])
-        self.assertEqual(resolve(path).view_name, 'produto:update_produto')
-        self.assertEqual(resolve(path).func, produto_update_view)
-    
-    def test_delete_url(self):
-        produto = Produto.objects.create(
-            nome='Capinha de smartphone',
-            descricao='Capinha de smartphone muito boa',
-            cod_barras='123456789088',
-            preco=11.99,
-            categoria='Produtos diversos'        
-        )
-        
-        path = reverse('produto:delete_produto', args=[produto.pk])
-        self.assertEqual(resolve(path).view_name, 'produto:delete_produto')
-        self.assertEqual(resolve(path).func, produto_delete_view)
->>>>>>> ade1cd5856bba61e60f916571a9c5ed28f9ea520
 
     def test_excluir_produto_url_resolves(self):
         url = reverse('produto:excluir_produto', args=[1])
--- projeto/produtosApp/views.py
+++ projeto/produtosApp/views.py
@@ -1,41 +1,23 @@
-<<<<<<< HEAD
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect # type: ignore
 from .models import Produto
 from .forms import ProdutoForm
-from django.contrib import messages
-from django.urls import reverse
+from django.contrib import messages # type: ignore
+from django.urls import reverse # type: ignore
 
 # Função para criar um produto
 def criar_produto(request):
-=======
-from django.shortcuts import render, redirect, get_object_or_404  # type: ignore
-from .models import Produto
-from .forms import ProdutoForm
-from django.contrib import messages  # type: ignore
-from django.urls import reverse  # type: ignore
-
-# Função para criar um aparelho
-
-
-def produto_create_view(request):
->>>>>>> ade1cd5856bba61e60f916571a9c5ed28f9ea520
     if request.method == 'POST':
         form = ProdutoForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Produto criado com sucesso!')
-<<<<<<< HEAD
             return redirect(reverse('produtos:listar_produtos'))  # Corrigido para incluir o app_name
-=======
-            return redirect(reverse('produto:list_produto'))
->>>>>>> ade1cd5856bba61e60f916571a9c5ed28f9ea520
     else:
         form = ProdutoForm()
 
     context = {
         'form': form
     }
-<<<<<<< HEAD
     return render(request, 'criar_produto.html', context)
 
 # Função para listar produtos
@@ -52,64 +34,28 @@ def visualizar_produto(request, pk):
 
 # Função para editar um produto
 def editar_produto(request, pk):
-=======
-    return render(request, 'produto_form.html', context)
-
-# Função para listar aparelhos
-
-
-def produto_list_view(request):
-    produtos = Produto.objects.all()
-    context = {'produtos': produtos}
-    return render(request, 'produto_list.html', context)
-
-# Função para atualizar um aparelho
-
-
-def produto_update_view(request, pk):
->>>>>>> ade1cd5856bba61e60f916571a9c5ed28f9ea520
     produto = get_object_or_404(Produto, pk=pk)
     if request.method == 'POST':
         form = ProdutoForm(request.POST, instance=produto)
         if form.is_valid():
             form.save()
             messages.success(request, 'Produto alterado com sucesso!')
-<<<<<<< HEAD
             return redirect(reverse('produtos:listar_produtos'))  # Corrigido para incluir o app_name
-=======
-            return redirect(reverse('produto:list_produto'))
->>>>>>> ade1cd5856bba61e60f916571a9c5ed28f9ea520
     else:
         form = ProdutoForm(instance=produto)
 
     context = {
         'form': form
     }
-<<<<<<< HEAD
     return render(request, 'editar_produto.html', context)
 
 # Função para excluir um produto
 def excluir_produto(request, pk):
-=======
-    return render(request, 'produto_update.html', context)
-
-# Função para excluir um aparelho
-
-
-def produto_delete_view(request, pk):
->>>>>>> ade1cd5856bba61e60f916571a9c5ed28f9ea520
     produto = get_object_or_404(Produto, pk=pk)
     if request.method == 'POST':
         produto.delete()
         messages.success(request, 'Produto excluído com sucesso!')
-<<<<<<< HEAD
         return redirect(reverse('produtos:listar_produtos'))  # Corrigido para incluir o app_name
 
     context = {'produto': produto}
     return render(request, 'excluir_produto.html', context)
-=======
-        return redirect(reverse('produto:list_produto'))
-
-    context = {'produto': produto}
-    return render(request, 'produto_confirm_delete.html', context)
->>>>>>> ade1cd5856bba61e60f916571a9c5ed28f9ea520
