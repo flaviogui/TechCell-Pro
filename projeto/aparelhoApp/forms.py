21a27fbb2c5a02dc55ac7e668f4d9abbcbabf152
Gutemsam <thesam0606@gmail.com>
2024-08-23 13:33:50 -0300

    adicionando funcionalidades e corrigindo bugs de telas

--- projeto/aparelhoApp/forms.py
+++ projeto/aparelhoApp/forms.py
@@ -1,8 +1,12 @@
-from django import forms  # type: ignore
-from aparelhoApp.models import Aparelho  # type: ignore
-
+# forms.py
+from django import forms
+from aparelhoApp.models import Aparelho
+from clienteApp.models import Cliente
 
 class AparelhoForm(forms.ModelForm):
     class Meta:
         model = Aparelho
         fields = '__all__'
+        widgets = {
+            'cliente': forms.Select(attrs={'class': 'form-control'}),  # Adiciona um widget de seleção para o cliente
+        }
